a865b66efb7f8c27e5bec06c1fae14077b9539f1
Radin6 <nicoramos647@hotmail.com>
2023-10-27 16:18:03 +0200

    calculator added, favicon and title fixed

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,19 @@ function App() {
         <Projects>
           <Project
             title={'Web3 Bank Project v1'}
-            href={
-              'https://radin6.github.io/src/pages/bank-project/bank-project.html'
-            }
+            href={'./src/pages/bank-project/index.html'}
             text={
               'This is a simple Smart contract and Webpage that simulate a Bank.\
               Back-End: Solidity, Javascript, Front-End: Javascript, HTML and CSS'
             }
             imgurl={'./web3-bank.png'}
+            alt={'profile pic'}
+          />
+          <Project
+            title={'Calculator'}
+            href={'https://radin6.github.io/calculator-app/'}
+            text={'It is a Calculator made with React JS'}
+            imgurl={'./calculator.jpg'}
             alt={'profile pic'}
           />
         </Projects>
