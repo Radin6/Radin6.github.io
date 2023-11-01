315622cd225339e8615f05da0c7774526e0839e4
Radin6 <nicoramos647@hotmail.com>
2023-11-01 17:19:37 +0100

    footer and links added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,29 @@
+import Icons from '../../assets/Icons';
+import './Footer.css';
+
 export default function Footer() {
   return (
     <div className="footer">
-      <p></p>
+      <h3>Contact Me</h3>
+      <section className="footer-content">
+        <a
+          href="mailto:nicoramos647@hotmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Icons icon="email" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/nicolasadelramos/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Icons icon="linkedin" />
+        </a>
+        <a href="https://github.com/Radin6" target="_blank" rel="noreferrer">
+          <Icons icon="github" />
+        </a>
+      </section>
     </div>
   );
 }
