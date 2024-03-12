e980ff054cfacf41cda716c87420c4f34d4cb005
Radin6 <nicoramos647@hotmail.com>
2024-03-12 18:44:30 +0100

    interface children added

--- src/components/Badge/BadgeClear.tsx
+++ src/components/Badge/BadgeClear.tsx
@@ -1,4 +1,10 @@
-export function BadgeClear ({children}) {
+import { ReactNode } from "react";
+
+interface BadgeClearProps {
+  children?: ReactNode;
+}
+
+export function BadgeClear ({children} : BadgeClearProps) {
   return (
     <span className="text-red-800 border-[1px] border-red-800 font-semibold rounded-md p-1">{children}</span>
   )
