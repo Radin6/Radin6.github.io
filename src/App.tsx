a823974d10925abad25710652560694a66c94851
Radin6 <nicoramos647@hotmail.com>
2024-03-13 19:30:08 +0100

    text about me changed

--- src/App.tsx
+++ src/App.tsx
@@ -70,32 +70,30 @@ function Main() {
 
 function AboutMe() {
   return (
-    <section
-      id="about-me"
-      className="flex flex-col items-center"
-    >
+    <section id="about-me" className="flex flex-col items-center">
       <div className="flex flex-col mx-4 md:w-[60%]">
         <hr className="border-[1px] border-white w-full" />
         <h2 className="text-3xl text-white text-center">About Me</h2>
         <Article title="Past">
-          I have always liked exact sciences that's why I became chemistry
-          technician and then food industry technician. Despite I like there is
-          nothing to innovate in this area so I decided to transition to web
-          development as it is governed by rules like the exact sciences but is
-          open to innovation and creativity. Above all it is a changing world
-          and there is much to discover.
+          I have always liked exact sciences, that's why I became a Chemistry
+          Technician and then, a Food Industry Technician. Although I like them,
+          I felt there was not so much to innovate in this area, so I decided to
+          transition to Web Development, as it is governed by rules like the
+          exact sciences. This field/industry is open to innovation and
+          creativity. Above all, it is a changing world and there is so much to
+          discover.
         </Article>
         <Article title="Present">
-          I am currently working on a project called Servi that is a marketplace
-          for freelance job and at the same time I am studying a full stack
-          development course specialized in blockchain. I’m also working on a
-          course teaching individually and in groups about blockchain technology
-          and how to interact with it.
+          I am currently working on a project called Servi, which is a
+          marketplace for freelance jobs. At the same time, I am studying Full
+          Stack Development specializing in blockchain. I’m also working on a
+          course. I teach individual and group lessons about blockchain
+          technology and how to interact with it.
         </Article>
         <Article title="Future">
-          In the future I find myself specializing in branches of development
-          that interest me since I like continuous learning. Especially by
-          bringing that knowledge into projects you’re working on.
+          In the future, I find myself specializing in branches of development
+          that interest me since I enjoy continuous learning. Especially, I see
+          myself bringing that knowledge into projects I am part of.
         </Article>
       </div>
     </section>
